8e10c69159e988fadee700f8792e4fc72eba8252
Anton Ashkolunenko <58958322+bashka411@users.noreply.github.com>
2022-10-11 13:24:45 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_work_ua_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_work_ua_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Course from Work.ua')),
+      body: const Center(
+        child: Text('Hello World!',
+        style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
